9f004b51adde490e3b46201374be98ea21ffb2a6
YashThakore <yashthakore50@yahoo.com>
2024-12-24 18:29:10 -0500

    Update spotifyAuth.js

--- src/spotifyAuth.js
+++ src/spotifyAuth.js
@@ -12,7 +12,7 @@ const scopes = [
 
 export const getAuthUrl = () => {
   const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-  const redirectUri = "http://localhost:3000/callback";
+  const redirectUri = "https://spotifyprofileandshuffle.vercel.app/callback";
   
   return `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
 };
